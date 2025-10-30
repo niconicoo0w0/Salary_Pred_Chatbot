27856c0cbd330bde723919f6fe80d8a0ed425730
niconicoo0w0 <zhilanw2@illinois.edu>
2025-10-30 17:44:17 -0400

    fix test coverage

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -1,8 +1,8 @@
 pytest -q \
   --maxfail=1 \
   --disable-warnings \
-  --cov=app \
-  --cov=utils \
+  --cov \
+  --cov-config=.coveragerc \
   --cov-report=term-missing \
   --cov-report=html \
   --cov-fail-under=90
